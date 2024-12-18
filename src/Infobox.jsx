e840826b56b66078734db375a86909898d0b086d
rsm09 <161129741+rsm09@users.noreply.github.com>
2024-12-18 20:44:09 +0530

    Update Infobox.jsx

--- src/Infobox.jsx
+++ src/Infobox.jsx
@@ -2,6 +2,9 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
+import ThunderstormIcon from '@mui/icons-material/Thunderstorm';
+import AcUnitIcon from '@mui/icons-material/AcUnit';
+import WbSunnyIcon from '@mui/icons-material/WbSunny';
 export default function Infobox({info}){
 
     let Rainy_img="https://plus.unsplash.com/premium_photo-1671406233410-9727cf249910?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
@@ -22,7 +25,12 @@ export default function Infobox({info}){
       />
       <CardContent>
         <Typography gutterBottom variant="h5" component="div">
-          {info.name}
+          {info.name}&nbsp;{
+              info.humidity>80
+              ?<ThunderstormIcon/>
+              :info.temp>20
+              ?<WbSunnyIcon/>:<AcUnitIcon/>
+        }
         </Typography>
         <Typography variant="body2" sx={{ color: 'text.secondary' }}>
           <p>Temperature = {info.temp}&deg;C</p>
